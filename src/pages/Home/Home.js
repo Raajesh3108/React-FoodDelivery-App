123a2fd52d873b76388fd8676f2b1a3bda95d069
Raajesh3108 <prajapatirajesh2003@gmail.com>
2024-03-26 01:10:12 +0530

    React food hero section css implementation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from "../../components/Layouts/Layout";
+import "../../styles/HomeStyle.css";
 import Section1 from './Section1';
 
 function Home() {
--- src/pages/Home/Section1.js
+++ src/pages/Home/Section1.js
@@ -14,7 +14,7 @@ const Section1 = () => {
                             <div className="price_badge">
                                 <div className="badge_text">
                                     <h4 className="h4_xs">Only</h4>
-                                    <h4 className="h3_lg">$49.99</h4>
+                                    <h4 className="h3_lg">$9.89</h4>
                                 </div>
                             </div>
                         </div>
