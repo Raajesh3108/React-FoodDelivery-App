4eb9fd0079f1569004070a05a724579f0eb4c4b5
Raajesh3108 <prajapatirajesh2003@gmail.com>
2024-04-02 00:55:08 +0530

    React food footer components

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,10 +1,96 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom';
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <footer>
+        <Container>
+          <Row>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Location</h5>
+                <p>158, Old Tanga Stand</p>
+                <p>Devika Sadar Bazar</p>
+                <p>New Delhi</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Working Hours</h5>
+                <p>Mon-Fri: 10:00AM - 11:00PM</p>
+                <p>Saturday: 10:00AM - 9:30PM</p>
+                <p>Sunday: 11:00AM - 6:00PM</p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Order Now</h5>
+                <p>Pizza</p>
+                <p>Drinks</p>
+                <p>Desserts</p>
+                <p>
+                  <Link to="tel:1800022022" className="calling">
+                    1800-022-022
+                  </Link>
+                </p>
+              </div>
+            </Col>
+            <Col sm={6} lg={3} className="mb-4 mb-lg-0">
+              <div className="text-center">
+                <h5>Follow Us</h5>
+                
+                <ul className="list-unstyled text-center mt-2">
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-facebook"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-twitter"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-instagram"></i>
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">
+                      <i class="bi bi-youtube"></i>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+          <Row className="copy_right">
+            <Col>
+              <div>
+                <ul className="list-unstyled text-center mb-0">
+                  <li>
+                    <Link to="/">
+                      © 2024 <span>Pizza Hut India</span>. All Rights Reserved
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Terms of Use</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Privacy Policy</Link>
+                  </li>
+                </ul>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </>
   )
 }
 
