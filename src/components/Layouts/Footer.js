51823a875efd23b1b859498388ca5579513d5028
Raajesh3108 <prajapatirajesh2003@gmail.com>
2024-04-02 01:11:43 +0530

    React food scroll function to working project

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 
 function Footer() {
+  // Scroll State
+  const [isVisible, setIsVisible] = useState(false);
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  const listenToScroll = () => {
+    let heightToHidden = 250;
+    const windowScroll = document.body.scrollTop || document.documentElement.scrollTop;
+
+    windowScroll > heightToHidden ? setIsVisible(true) : setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+  });
+
   return (
     <>
       <footer>
@@ -90,6 +110,13 @@ function Footer() {
           </Row>
         </Container>
       </footer>
+
+      {/* Sroll To Top */}
+      {isVisible && (
+        <div className="scroll_top" onClick={scrollTop}>
+          <i class="bi bi-arrow-up"></i>
+        </div>
+      )}
     </>
   )
 }
